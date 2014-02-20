21f75120d7f8d91cc208c0012d5c6e4bc2e96c83
André König <andre.koenig@gmail.com>
2014-02-20 11:40:06 +0100

    Bumped version.

--- angular-deckgrid.js
+++ angular-deckgrid.js
@@ -1,4 +1,4 @@
-/*! angular-deckgrid (v0.3.0) - Copyright: 2013 - 2014, André König (andre.koenig@posteo.de) - MIT */
+/*! angular-deckgrid (v0.3.1-dev) - Copyright: 2013 - 2014, André König (andre.koenig@posteo.de) - MIT */
 /*
  * angular-deckgrid
  *
