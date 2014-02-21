a4b677efc84ea9c21d8fdc1b020a13add64dd7a3
Sebastian Oergel <sebastian.oergel@student.hpi.uni-potsdam.de>
2014-02-21 11:08:08 +0100

    Add possibility to set card template with attribute string or with inner HTML instead of using an external template file.
    The card templates can now be provided by using a cardTemplateString attribute (which uses the attribute value as the template) or by omitting any template attributes (which uses the inner HTML of the directive as the template). Please see the extended README for details of the syntax and functionality. Specs for these new features are included. This should fix both aspects of issue #7.

--- specs/deckgrid.spec.js
+++ specs/deckgrid.spec.js
@@ -75,4 +75,80 @@ describe('Unit: The angular-deckgrid', function () {
             expect($($indices[2]).text()).toBe('2');
         }
     ]));
+
+    it('should render the inner html of the directive if no card template attribute is specified', inject([
+
+        '$rootScope',
+        '$compile',
+        '$window',
+
+        function ($rootScope, $compile, $window) {
+            var $photos,
+                $columns;
+
+            $window.getComputedStyle = function () {
+                return {
+                    content: '\''+ columnCount + ' .column.size-1-' + columnCount + '\''
+                };
+            };
+
+            $rootScope.photos = photos;
+
+            // 
+            // use inner html as the card template
+            //
+            $elem = $compile('<deckgrid class="deckgrid" source="photos">' +
+                '<img data-ng-src="{{card.src}}" alt="{{card.id}}" src=""></deckgrid>')($rootScope);
+
+            $rootScope.$digest();
+            
+            $photos = $elem.find('.column:first-child img');
+            $columns = $elem.find('.column');
+
+            expect($columns.length).toBe(columnCount);
+            expect($photos.length).toBe(3);
+
+            expect($($photos[0]).attr('alt')).toBe(photos[0].id);
+            expect($($photos[1]).attr('alt')).toBe(photos[3].id);
+            expect($($photos[2]).attr('alt')).toBe(photos[6].id);
+        }
+    ]));
+
+    it('should render the html of the provided cardTemplateString attribute if this is provided instead of the cardTemplate one', inject([
+
+        '$rootScope',
+        '$compile',
+        '$window',
+
+        function ($rootScope, $compile, $window) {
+            var $divs,
+                $columns;
+
+            $window.getComputedStyle = function () {
+                return {
+                    content: '\''+ columnCount + ' .column.size-1-' + columnCount + '\''
+                };
+            };
+
+            $rootScope.photos = photos;
+
+            // 
+            // use the provided attribute value
+            //
+            $elem = $compile('<deckgrid class="deckgrid" source="photos" ' +
+                'cardTemplateString="<p>{{card.id}}</p>"></deckgrid>')($rootScope);
+
+            $rootScope.$digest();
+            
+            $divs = $elem.find('.column:first-child p');
+            $columns = $elem.find('.column');
+
+            expect($columns.length).toBe(columnCount);
+            expect($divs.length).toBe(3);
+
+            expect($($divs[0]).text()).toBe(photos[0].id);
+            expect($($divs[1]).text()).toBe(photos[3].id);
+            expect($($divs[2]).text()).toBe(photos[6].id);
+        }
+    ]));
 });
