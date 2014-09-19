9ceae0843443dde586b9dc1458e268485c2668bf
Michel Boudreau <michelb@pageuppeople.com>
2014-09-19 14:34:25 +1000

    adding pull request 48

--- angular-deckgrid.js
+++ angular-deckgrid.js
@@ -387,7 +387,7 @@ angular.module('akoenig.deckgrid').factory('Deckgrid', [
             newModel = newModel || [];
             oldModel = oldModel || [];
 
-            if (oldModel.length !== newModel.length) {
+            if (!angular.equals(oldModel, newModel)) {
                 self.$$createColumns();
             }
         };
